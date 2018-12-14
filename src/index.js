81fd33ae1fb7b0111d3b43b1b5a2a10b1c71735e
Hang Wei <hangwei@aidigger.com>
2018-12-14 14:04:03 +0800

    rwst

--- src/index.js
+++ src/index.js
@@ -1,57 +1,59 @@
-import React from "react";
+import React,{ Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./styles.css";
 
-const App = () => (
-  <Router>
-    <div>
-      <Header />
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-);
+  render() {
+    return this.props.children;
+  }
+}
+
+class ScrollToTopOnMount extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+class LongContent extends Component {
+  render() {
+    <div>
+      <ScrollToTopOnMount />
+      <h1>Here is my long content page</h1>
+    </div>;
+  }
+}
+
 
 const Home = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
-const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
+const Always = ({ match }) => <h3>always</h3>;
+const NoMatch = () => <h1>404</h1>;
 
-    <ul>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
 
-    <Route path={`${match.path}/:id`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-const Header = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-    <li>
-      <Link to="/topics">Topics</Link>
-    </li>
-  </ul>
+
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/long-content" component={LongContent} />;
+
+      <Route component={NoMatch} />
+    </Switch>
+  </Router>
 );
 
 const rootElement = document.getElementById("root");
